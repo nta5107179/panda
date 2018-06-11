7d453adc7b204206c66bfe656736c9956e5d5246
Panda <panda@membrane-solutions.com>
2018-06-11 17:55:44 +0800

    a

--- Web/wwwroot/Admin/JS/Config.js
+++ Web/wwwroot/Admin/JS/Config.js
@@ -18,6 +18,35 @@ var static = {
         }
         return arr.join("&");
     },
+    oldBoxWidth: 0,
+    newBoxWidth: 0,
+    setTableStyle: function (variable_value)
+	{
+        var thList = $(".table thead tr th");
+        var widthSum = 0;
+        var _variable_value = variable_value; //可变宽度最小值
+        for (var i = 0; i < thList.length; i++)
+        {
+            if (thList[i].style.width != "auto")
+                widthSum += parseInt(thList[i].style.width.replace("px"));
+        }
+        static.newBoxWidth = $("#app ._list ._box").width();
+        if (static.oldBoxWidth != 0)
+        {
+            if (static.oldBoxWidth > static.newBoxWidth && static.newBoxWidth < widthSum + _variable_value)
+            {
+                $(".table").css("width", "max-content");
+                $("#_variable_th").css("width", _variable_value);
+            }
+            else if (static.oldBoxWidth < static.newBoxWidth && static.newBoxWidth > widthSum)
+            {
+                $(".table").css("width", "100%");
+                $("#_variable_th").css("width", "auto");
+            }
+        }
+        static.oldBoxWidth = static. newBoxWidth
+
+    },
     defalut:null
 }
 
